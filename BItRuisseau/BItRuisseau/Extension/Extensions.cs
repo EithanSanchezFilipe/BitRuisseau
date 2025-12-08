202952e5dd0bb13daf9de079042c09724d55af7d
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-12-08 10:23:23 +0100

    feat(extensions): add string utility extension [10][DONE]

---
+++ BItRuisseau/BItRuisseau/Extension/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BItRuisseau.Extensions
+{
+        public static class Extensions
+        {
+            public static string Shorten(this string text, int max)
+            {
+                if (string.IsNullOrEmpty(text))
+                    return text;
+
+                return text.Length <= max
+                    ? text
+                    : text.Substring(0, max) + "...";
+            }
+        }
+}
