ec97e4d67e013b2cd80c5e9cd325925dc11c46d8
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-12-15 15:19:56 +0100

    feat: add protocol models for MQTT communication [20m][done]

---
+++ BItRuisseau/BItRuisseau/Models/Envelope.cs
@@ -0,0 +1,33 @@
+﻿using BitRuisseau.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+namespace BitRuisseau.Models
+{
+    public class Envelope(string senderId, string receiverId, MessageType type, string? message)
+    {
+        public string Id { get; init; } = Guid.NewGuid().ToString();
+        public string SenderId { get; init; } = senderId;
+        public string? ReceiverId { get; init; } = receiverId;
+        public MessageType Type { get; init; } = type;
+        public string Message { get; set; } = message;
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public static Envelope? FromJson(string json)
+        {
+            return JsonSerializer.Deserialize<Envelope>(json);
+        }
+
+        public override string ToString()
+        {
+            return ToJson();
+        }
+    }
+}
